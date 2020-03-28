0f0bed9177c12d20f08b6952d9fdb991d3f6c52c
bintanglazuardi <bintanglazuardi@mail.ugm.ac.id>
2020-03-29 03:24:56 +0700

    Updates

---
+++ precache-manifest.f3573f985b4208a7882a9d22bccfd371.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aabe46fd4e3f23ee6940fa4ce383a96f",
+    "url": "/WEB3A-15702-BINTANGLAZUARDI-REACT/index.html"
+  },
+  {
+    "revision": "84f3ab8f11978393dc0c",
+    "url": "/WEB3A-15702-BINTANGLAZUARDI-REACT/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "fd8e480676698b58118f",
+    "url": "/WEB3A-15702-BINTANGLAZUARDI-REACT/static/css/main.071b367a.chunk.css"
+  },
+  {
+    "revision": "84f3ab8f11978393dc0c",
+    "url": "/WEB3A-15702-BINTANGLAZUARDI-REACT/static/js/2.116bd246.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/WEB3A-15702-BINTANGLAZUARDI-REACT/static/js/2.116bd246.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd8e480676698b58118f",
+    "url": "/WEB3A-15702-BINTANGLAZUARDI-REACT/static/js/main.a203d053.chunk.js"
+  },
+  {
+    "revision": "c5a1e6011ae320cf1403",
+    "url": "/WEB3A-15702-BINTANGLAZUARDI-REACT/static/js/runtime-main.877bb893.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/WEB3A-15702-BINTANGLAZUARDI-REACT/static/media/logo.ee7cd8ed.svg"
+  }
+]);
